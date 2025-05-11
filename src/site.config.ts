75d809efe2fb05c4ab75c569cfa50d24990c6839
FabrizzioCH <neto-capoleto@hotmail.com>
2025-05-11 14:28:16 -0600

    Agregué el temario y el primer iframe

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,43 +1,35 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Fabrizzio Cázares y Alexander Beltrán",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-	date: {
-		locale: "es-MX",
-		options: {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
-		},
-	},
-	// Used as the default description meta property and webmanifest description
-	description: "Sitio web para enseñar anatomía con modelos 3D",
-	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-	lang: "es-MX",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "es_MX",
-	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "CuerpoPop",
+  // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "Fabrizzio Cázares y Alexander Beltrán",
+  // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+  date: {
+    locale: "es-MX",
+    options: {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    },
+  },
+  // Used as the default description meta property and webmanifest description
+  description: "Sitio web para enseñar anatomía con modelos 3D",
+  // HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
+  lang: "es-MX",
+  // Meta property, found in src/components/BaseHead.astro L:42
+  ogLocale: "es_MX",
+  // Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
+  title: "CuerpoPop",
 };
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: { path: string; title: string }[] = [
-	{
-		path: "/",
-		title: "Inicio",
-	},
-	{
-		path: "/about/",
-		title: "Acerca de",
-	},
-	{
-		path: "/posts/",
-		title: "Blog",
-	},
-	{
-		path: "/notes/",
-		title: "Notas",
-	},
+  {
+    path: "/",
+    title: "Inicio",
+  },
+  {
+    path: "/temario",
+    title: "Temario",
+  },
 ];
